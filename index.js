0916b1b9fce540eac14e402a7c461de089a2475b
ahmad1101 <66177703+ahmad1101@users.noreply.github.com>
2022-08-27 20:13:56 +0500

    Save functionality added

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
 let card = document.querySelector('.card');
-let cardArr = [];
 
 let body = document.body;
     card.addEventListener('click',  (e)=>{
@@ -10,16 +9,37 @@ let body = document.body;
     let div = document.createElement('div');
     div.classList.add('content');
     
-    cardArr.push(val.value);
-    div.innerText = val.value;
-    card.append(div);
-    btn.remove();
-    val.remove();
-    html='<input type="text" class="inp" placeholder="Add food item">\
-    <input type="button" class="btn" value="Add item">'
-    card.innerHTML += html;
-    }
     
+    if(val.value == '')
+    {
+        alert("Invalid input!");
+    }
+
+    else 
+    {
+        div.innerText = val.value;
+        card.append(div);
+        btn.remove();
+        val.remove();
+        html='<input type="text" class="inp" placeholder="Add food item">\
+        <input type="button" class="btn" value="Add item">'
+        card.innerHTML += html;
+    }
+  
+    }
 });
 
-console.log(cardArr);
+let save = document.querySelector('.butn');
+save.addEventListener('click', saveFunc = () =>{
+    localStorage.setItem('card', card.innerHTML);
+    let saved = localStorage.getItem('card');
+    if(saved)
+    {
+        card.innerHTML = saved
+    }
+
+    document.querySelector('.inp').setAttribute('style', 'display:none');
+    document.querySelector('.btn').setAttribute('style', 'display:none');
+})
+
+
